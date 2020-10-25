68230c5478bd7965a3d7a328109e213ccb8f44c7
Arseniy Sleptsov <arseniy.sleptsov@gmail.com>
2020-10-25 19:59:12 +0300

    update 25 Oct

---
+++ webapp/districts_data.py
@@ -0,0 +1,23 @@
+import xlrd
+
+loc = ("webapp/regions.xlsx")
+
+wb = xlrd.open_workbook(loc)
+sheet = wb.sheet_by_index(0)
+
+region_list = {}
+
+def type_of_region():
+    for region in range(sheet.nrows):
+        region_var = sheet.cell(region,1).value
+        type_color = sheet.cell(region,4).value
+        region_list[region_var] = type_color
+    region_list.pop('Region code')
+    return region_list
+
+def region_borders():
+    for region in range(sheet.nrows):
+        region_var = sheet.cell(region,1).value
+        region_list[region_var] = '#48C9B0'
+    region_list.pop('Region code')
+    return region_list
